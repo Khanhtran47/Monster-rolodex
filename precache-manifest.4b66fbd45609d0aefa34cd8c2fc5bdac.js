5b28c937bc751d774188d525df7cca6aea8893d4
Khanhtran47 <tranduckhanh23@gmail.com>
2020-07-11 02:58:14 +0200

    Updates

---
+++ precache-manifest.4b66fbd45609d0aefa34cd8c2fc5bdac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5367b04deb25edc8a30dcac76c20c6a4",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "3ba3ad0b3e1a819cd522",
+    "url": "/Monster-rolodex/static/css/main.fa1db9fc.chunk.css"
+  },
+  {
+    "revision": "82815bfcb9bf73d8a971",
+    "url": "/Monster-rolodex/static/js/2.be6af004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-rolodex/static/js/2.be6af004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba3ad0b3e1a819cd522",
+    "url": "/Monster-rolodex/static/js/main.a3d42c28.chunk.js"
+  },
+  {
+    "revision": "577121aeee2ba7cc45d7",
+    "url": "/Monster-rolodex/static/js/runtime-main.b1d8ffe6.js"
+  }
+]);
